fdfbbe45720b164b9a042573f39569fb4413cfe6
Kevin Siml <appzer@users.noreply.github.com>
2020-02-07 16:27:21 +0100

    Update Pushsafer.cpp

--- src/Pushsafer.cpp
+++ src/Pushsafer.cpp
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2017 Brian Lough. All right reserved.
-Copyright (c) 2019 Pushsafer.com Kevin Siml
+Maintained by Pushsafer.com (Kevin Siml)
 
 Pushsafer - Library to send Pushsafer notifications.
 Ref. Library at https:github/esp8266/Arduino
